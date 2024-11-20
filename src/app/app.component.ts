59eecba5db17093916f6c089c127a2579a7b3290
imane-daouah <imane.daouah@gmail.com>
2024-11-20 19:54:11 +0100

    Sauvegarde des modifications avant mise à jour Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'car_rental_angular';
+  title = 'car-rental-angular'; // Nom de l'application
 }
