f2d62ca2fa5712dbe2d5811ee436807e52288f5b
imane-daouah <imane.daouah@gmail.com>
2024-11-23 16:37:50 +0100

    cree page login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    // Initialisation du formulaire
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      console.log('Données soumises :', loginData); // Affiche les données dans la console
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
+
+  navigateToSignup(): void {
+    this.router.navigate(['/register']); // Redirige vers la page d'inscription
+  }
 }
