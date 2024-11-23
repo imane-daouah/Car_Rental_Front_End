ba5fd6bc41fd7527cf6538822bc628f1265d977b
imane-daouah <imane.daouah@gmail.com>
2024-11-23 16:23:07 +0100

    cree page login

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -11,17 +11,35 @@ import { RouterModule } from '@angular/router';
 })
 export class SignupComponent {
   signupForm: FormGroup;
+  authService: any;
+  router: any;
 
   constructor(private fb: FormBuilder) {
     this.signupForm = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required]],
-      name: ['', [Validators.required]]
+      numero_tel: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      userRole: ['USER', Validators.required],
+      adresse: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4)]],
     });
   }
 
   onSubmit(): void {
-    console.log(this.signupForm.value);
+    if (this.signupForm.valid) {
+      const userData = this.signupForm.value;
+      this.authService.registerUser(userData).subscribe(
+        (response: any) => {
+          console.log('Utilisateur créé avec succès', response);
+          this.router.navigate(['/login']); // Redirige vers la page de connexion après succès
+        },
+        (error: any) => {
+          console.error('Erreur lors de la création de l\'utilisateur', error);
+        }
+      );
+    } else {
+      console.log('Formulaire invalide');
+    }
   }
 }
--- src/app/auth/services/auth/auth.service.ts
+++ src/app/auth/services/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:8080/api/users'; // URL de votre backend Spring Boot
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Méthode pour enregistrer un utilisateur
+  registerUser(userData: any): Observable<any> {
+    return this.http.post(this.apiUrl, userData);
+  }
 }
